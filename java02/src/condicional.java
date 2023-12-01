c8c4927cdf69719f018ca533caa37717778a2408
eraldo06 <eraldoleonardo384@gmail.com>
2023-12-01 15:31:31 -0300

    condicional

---
+++ java02/src/condicional.java
@@ -0,0 +1,19 @@
+public class condicional {
+    public static void main(String[] args){
+
+        int a, b;
+        a = 6;
+        b = 7;
+
+        /*String resultado = "";
+        if(a==b)
+        resultado = "VERDADEIRO";
+        else
+        resultado = "FALSO";*/
+
+
+        String resultado = a==b ?"VERDADEIRO" : "FALSO";
+
+        System.err.println(resultado);
+    }
+}
