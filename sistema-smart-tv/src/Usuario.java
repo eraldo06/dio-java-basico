aeaf639dc8654fa3f4ec149112934b044795890b
eraldo06 <eraldoleonardo384@gmail.com>
2023-12-01 20:47:53 -0300

    metedos na tv

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -2,10 +2,17 @@ public class Usuario {
     public static void main(String[] args) throws Exception {
         
         SmartTv smartTv = new SmartTv();
+        smartTv.mudarCanal(99);
 
         System.out.println("TV Ligada? " + smartTv.ligada);
         System.out.println("Canal Atual: " + smartTv.canal);
         System.out.println("Volume Atual: " +smartTv.volume);
+
+        smartTv.ligar();
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+
+
 
     }
 }
