774cb13aaf1ec843fbdfba60c5af09e33ba6f2f2
eraldo06 <eraldoleonardo384@gmail.com>
2023-12-05 23:22:09 -0300

    trabalhando controle de fluxo

---
+++ aulas/controle de fluxo/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Double saldo = 25.0;
+        double valorSolicitado = 17.0;
+
+        if(valorSolicitado < saldo)
+        saldo = saldo - valorSolicitado;
+
+        System.out.println(saldo);
+    }
+}
---
+++ aulas/controle de fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,13 @@
+public class ResultadoEscolar {
+
+    public static void main(String[] args) throws Exception {
+       int nota = 6;
+
+        if(nota>=7)
+        System.out.println("aprovado");
+        else
+        System.out.println("reprovado");
+    
+    }
+    
+}
