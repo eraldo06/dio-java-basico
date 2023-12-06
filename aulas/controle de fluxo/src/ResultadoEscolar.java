9783b4c7d03c61150fcc1d647bebd7a17f78d26d
eraldo06 <eraldoleonardo384@gmail.com>
2023-12-06 08:10:13 -0300

    estrutura condicionais

--- aulas/controle de fluxo/src/ResultadoEscolar.java
+++ aulas/controle de fluxo/src/ResultadoEscolar.java
@@ -1,12 +1,29 @@
 public class ResultadoEscolar {
 
     public static void main(String[] args) throws Exception {
-       int nota = 6;
+       int nota = 4;
 
+
+       //primeiro jeito de fazer
         if(nota>=7)
         System.out.println("aprovado");
+
+        else if(nota >=5 && nota <7)
+        System.out.println("recuperação");
+
         else
         System.out.println("reprovado");
+
+
+
+        // segundo jeito de fazer
+        String resultado = nota >=7 ? "aprovado" : "reprovado";
+        System.out.println(resultado);
+
+
+        // terceiro jeito de fazer
+        String resultado1 = nota >=7 ? "aprovado" : nota >=5 && nota <7 ? "recuperação" : "reprovado";
+        System.out.println(resultado1);
     
     }
     
