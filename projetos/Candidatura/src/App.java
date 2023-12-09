b8496f1710ac46936b16dc31a6ca95ed99c3bc8e
eraldo06 <eraldoleonardo384@gmail.com>
2023-12-09 16:05:40 -0300

    sistema candidato

--- projetos/Candidatura/src/App.java
+++ projetos/Candidatura/src/App.java
@@ -2,10 +2,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class App {
     public static void main(String[] args) {
-      /*   analisarCandidato(1999.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
-        */
+        
         selecaoCandidatos();
 
     }
@@ -37,15 +34,5 @@ public class App {
         return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
-/* 
-    static void analisarCandidato(double salarioPretendido){
-        double salarioBase = 2000.0;
-        if (salarioBase > salarioPretendido){
-            System.out.println("LIGAR PARA O CANDIDATO");
-        }else if(salarioBase==salarioPretendido)
-        System.out.println("LIGAE PARA O CANDIDATO COM CONTRA PROPOSTA");
-        else{
-            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
-        }
-    } */
+
 }
